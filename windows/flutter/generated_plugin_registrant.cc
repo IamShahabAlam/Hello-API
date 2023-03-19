05e1b959749f2bc3103c01c150e2e73636f11c1b
Shahab Alam <iamShahabAlam@gmail.com>
2023-03-19 23:42:51 +0500

    compilesdkversion 33

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <modal_progress_hud_nsn/modal_progress_hud_nsn_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  ModalProgressHudNsnPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ModalProgressHudNsnPlugin"));
 }
